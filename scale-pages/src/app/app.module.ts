3dcf7f805fa65306383ad56917fb7be744bb488c
Carlos Mocholí <cmocholi@okode.com>
2017-11-29 10:24:30 +0100

    Remove unnecessary imports

--- scale-pages/src/app/app.module.ts
+++ scale-pages/src/app/app.module.ts
@@ -1,14 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu';
-import { LoginModule } from '../pages/login/login.module';
-import { ScalesModule } from '../pages/scales/scales.module';
-import { SettingsModule } from '../pages/settings/settings.module';
 import { ScalesComponent } from '../pages/scales/scales';
 
 @NgModule({
@@ -19,9 +14,6 @@ import { ScalesComponent } from '../pages/scales/scales';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    LoginModule,
-    ScalesModule,
-    SettingsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
